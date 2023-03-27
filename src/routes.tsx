0f70d5b6dd9c531ec1f031956552f316565323d3
Maique Doglas Moraes da Silva <maiqued.18@gmail.com>
2023-03-26 21:57:10 -0300

    feat(routes): add routes and theme

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PersonalWallet from "./pages/PersonalWallet";
+import AllCrypto from "./pages/AllCrypto";
+
+const AppRoutes = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PersonalWallet />} />
+            <Route path="/cryptos" element={<AllCrypto />} />
+        </Routes>
+    </BrowserRouter>
+);
+
+export default AppRoutes;
